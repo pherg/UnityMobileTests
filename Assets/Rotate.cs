096b8eb853bf2e21ac029fae374109dfd929a778
Pherg <pherguson@gmail.com>
2016-02-28 16:48:38 -0800

    Simple scene and test script.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	
+	void Update () {
+        transform.Rotate(transform.InverseTransformDirection(0, 1, 0), 2.4f);
+	}
+}
